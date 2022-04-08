d7534b25220264b73f3af33069a88b9e3f87b55c
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2022-04-08 17:14:15 +0300

    Добавьте файлы проекта.

---
+++ TreeNode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TreeNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode n = new TreeNode('n');
+            TreeNode m = new TreeNode('m');
+
+            TreeNode l = new TreeNode('l');
+            TreeNode k = new TreeNode('k') { Children = new TreeNode[] { n } };
+            TreeNode j = new TreeNode('j') { Children = new TreeNode[] { m } };
+            TreeNode i = new TreeNode('i');
+
+            TreeNode h = new TreeNode('h') { Children = new TreeNode[] { l } };
+            TreeNode g = new TreeNode('g');
+            TreeNode f = new TreeNode('f') { Children = new TreeNode[] { k } };
+            TreeNode e = new TreeNode('e');
+            TreeNode d = new TreeNode('d') { Children = new TreeNode[] { i, j } };
+
+            TreeNode b = new TreeNode('b') { Children = new TreeNode[] { d, e, f } };
+            TreeNode c = new TreeNode('c') { Children = new TreeNode[] { g, h } };
+
+            TreeNode a = new TreeNode('a') { Children = new TreeNode[] { b, c } };
+
+            //обход в ширину
+            var chidlren = TreeNode.BFS(a);
+            foreach (var temp in chidlren)
+                Console.WriteLine(temp.Value);
+
+            Console.WriteLine();
+
+            //прямой обход
+            chidlren = TreeNode.NLR(a);
+            foreach (var temp in chidlren)
+                Console.WriteLine(temp.Value);
+
+        }
+    }
+}
---
+++ TreeNode/TreeNode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+namespace TreeNode
+{
+    public sealed class TreeNode
+    {
+        public char Value { get; set; }
+        public TreeNode(char s)
+        {
+            Value = s;
+        }
+        public IEnumerable<TreeNode> Children { get; init; }
+        //Поиск в ширину
+        public static IEnumerable<TreeNode> BFS(TreeNode root)
+        {
+            //лист содержит всех потомков включая корневой узел
+            List<TreeNode> answer = new List<TreeNode>();
+            
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            //добавим главный узел в очередь
+            queue.Enqueue(root);
+            
+            while(queue.Count > 0)
+            {
+                //получим самый старый элемент очереди
+                TreeNode node = queue.Dequeue();
+                //добавим его в ответ
+                answer.Add(node);
+                //если у него есть дети
+                if (node.Children is not null)
+                {
+                    //сделаем перебор по ним и добавим их в очередь
+                    foreach (var child in node.Children)
+                    {
+                        if (child is not null)
+                            queue.Enqueue(child);
+                    }
+                }
+            }
+            return answer;
+        }
+        //Прямой обход
+        public static IEnumerable<TreeNode> NLR(TreeNode node)
+        {
+            //лист содержит всех потомков включая корневой узел
+            List<TreeNode> answer = new List<TreeNode>();
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(node);
+            while (stack.Count > 0)
+            {
+                var tempnode = stack.Pop();
+                answer.Add(tempnode);
+                
+                //если у него есть дети
+                if (tempnode.Children is not null)
+                {
+                    //сделаем перебор по ним и добавим их в стек
+                    foreach (var child in tempnode.Children)
+                    {
+                        if (child is not null)
+                            stack.Push(child);
+                    }
+                }
+            }
+            return answer;
+        }
+
+    }
+}
